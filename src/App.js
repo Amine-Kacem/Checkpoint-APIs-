a7ab9a099f9cec48ff64bdb97ffccee896c043df
unknown <kacem.amine1112@gmail.com>
2020-09-10 23:38:07 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import { Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import UserCard from "./Components/UserCard";
+import UsersInfo from "./Components/UsersInfo";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    users: [],
+    posts: [],
+    comments: [],
+  };
+
+  getAllUsers = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => this.setState({ users: res.data }))
+      .catch((error) => {
+        console.error(error);
+        alert("Can not fetch users list");
+      });
+  };
+
+  componentDidMount = () => {
+    this.getAllUsers();
+    this.getPostsById();
+  };
+
+  getPostsById = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => this.setState({ posts: res.data }))
+      .catch((error) => {
+        console.error(error);
+        alert("Can not fetch user's posts");
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+
+        <div className="App">
+          <Route
+            exact
+            path="/"
+            render={() =>
+              this.state.users.map((el, i) => <UserCard key={i} user={el} />)
+            }
+          />
+        </div>
+
+        {this.state.users.map((user) => (
+          <Route
+            exact
+            path={`/${user.id}`}
+            render={() => (
+              <UsersInfo
+                key={user.id}
+                user={user}
+                posts={this.state.posts.filter(
+                  (post) => post.userId === user.id
+                )}
+              />
+            )}
+          />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
